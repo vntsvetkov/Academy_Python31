8a00d0c2992397d9e919ac1f3ba7b22b37417f24
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-10-23 23:19:02 +0300

    Добавил правки в методы класса окружность

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ def main():
     circle1 = Circle(1, 2, 3)
     circle2 = Circle(3, 4, 6)
 
-    circle3 = Circle.create_circle_by_diameter(1, 2, 8)
-    circle4 = Circle.create_circle_by_diameter(3, 4, 10)
+    circle3 = Circle.create_by_diameter(1, 2, 8)
+    circle4 = Circle.create_by_diameter(3, 4, 10)
 
     print(Circle.get_count())
 
-    circle1.obj_set_color('#00FF00')   # Изменить цвет только circle1
+    circle1.color = '#00FF00'   # Изменить цвет только circle1
     Circle.set_color('#FF0000')   # Изменить цвет всем объектам класса Circle
 
-    print(circle1.__dict__)  # Тут есть параметр color
-    print(circle2.__dict__)  # Тут нет параметра color
+    print(circle1.color)  # Тут есть параметр color
+    print(circle2.color)  # Тут нет параметра color
 
     # current_sum_rub = 1000
     # ConverterCurrency.dollar_rate = 100
--- module.py
+++ module.py
@@ -13,13 +13,13 @@ from math import pi
 
 3. Методы класса - методы, которые можно вызывать от самого класса.
 Этот метод не может влиять на состояние объекта, но влияет на состояние класса
-Основная задача 
+Основная задача - альтернативный конструктор
 """
 
 
 class Circle:
 
-    __color: str = '#FFFFFF'  # цвет фигуры
+    __color: str = '#0000FF'  # цвет фигуры по умолчанию
     __count: int = 0  # количество экземпляров объекта
 
     def __init__(self, x: int, y: int, radius: float):
@@ -28,16 +28,22 @@ class Circle:
         self.__radius = self.__validate_radius(radius)
         Circle.__count += 1
 
-    def obj_set_color(self, value):
-        self.__color = value
-
-    def obj_get_color(self):
-        return self.__color
-
     @classmethod
-    def create_circle_by_diameter(cls, x: int, y: int, diameter: float):
+    def create_by_diameter(cls, x: int, y: int, diameter: float):
         radius = cls.__validate_radius(diameter / 2)
         return cls(x, y, radius)
+
+    @property
+    def color(self):
+        return self.__color
+
+    @color.setter
+    def color(self, value):
+        self.__color = value
+
+    @classmethod
+    def get_count(cls):
+        return cls.__count
 
     @classmethod
     def get_color(cls):
@@ -54,10 +60,6 @@ class Circle:
     @property
     def area(self):
         return pi * self.__radius ** 2
-
-    @staticmethod
-    def get_count():
-        return Circle.__count
 
     @staticmethod
     def __validate_radius(radius: float):
