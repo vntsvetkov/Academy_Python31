703826401a9c26d7b33d93f04bde43fd34bf5297
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-11-13 20:57:49 +0300

    Добавил материалы по методам сравнения

--- main.py
+++ main.py
@@ -2,7 +2,32 @@ from module import *
 
 
 def main():
-    ...
+    # Создание собственной последовательности
+    # stack = Stack()
+    # stack.append(1)
+    # stack.append(2)
+    # print(len(stack))
+
+    # Пример использования контекстного менеджера
+    # with Timer() as timer:
+        # Ваш блок кода
+        # time.sleep(1)
+
+    # Создать список моментов времени и упорядочить их по возрастанию
+
+    clock1 = Clock(0, 10, 0)
+    clock2 = Clock(0, 8, 0)
+    clock3 = Clock(0, 9, 0)
+
+    clocks = [clock1, clock2, clock3]
+    clocks = sorted(clocks, reverse=True)
+    print(clocks)
+
+    # clock = {
+    #     clock1: "Полдень",
+    #     clock2: "Полдень",
+    # }
+    # print(clock)
 
 
 if __name__ == "__main__":
---
+++ module.py
@@ -0,0 +1,130 @@
+""" Магические (дандеры) методы """
+import time
+
+
+# Последовательности
+class StackIndexError(IndexError):
+
+    def __init__(self, text):
+        self.text = text
+
+
+class Stack(object):
+    def __init__(self):
+        self._floors = []
+
+    def __setitem__(self, floor_number, data):
+        self._floors[floor_number] = data
+
+    def __getitem__(self, floor_number):
+        if len(self._floors) <= abs(floor_number):
+            raise StackIndexError("stack index out of range")
+        return self._floors[floor_number]
+
+    def __len__(self):
+        return len(self._floors)
+
+    def append(self, data):
+        self._floors.append(data)
+
+    def __bool__(self):
+        return len(self) != 0
+
+# Менеджер контекста
+class Timer:
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        elapsed_time = time.time() - self.start_time
+        print(f"Elapsed time: {elapsed_time} seconds")
+
+
+class Clock:
+    """ Класс, который хранит момент времени """
+
+    def __init__(self, h: int, m: int, s: int):
+        self.__hour = h
+        self.__minute = m
+        self.__second = s
+
+    def __repr__(self):
+        return f"{self.__hour // 10}{self.__hour % 10}:" \
+               f"{self.__minute // 10}{self.__minute % 10}:" \
+               f"{self.__second // 10}{self.__second % 10}"
+
+    def __str__(self):
+        return f"{self.__hour // 10}{self.__hour % 10}:" \
+               f"{self.__minute // 10}{self.__minute % 10}:" \
+               f"{self.__second // 10}{self.__second % 10}"
+
+    def to_seconds(self):
+        return self.__hour * 3600 + self.__minute * 60 + self.__second
+
+    def __lt__(self, other):
+        if isinstance(other, (Clock, int)):
+            if isinstance(other, int):
+                return self.to_seconds() < other
+            return self.to_seconds() < other.to_seconds()
+        else:
+            raise TypeError(f"'<' not supported between instances "
+                            f"of Clock and {other.__class__.__name__}")
+
+    def __le__(self, other):
+        if isinstance(other, (Clock, int)):
+            if isinstance(other, int):
+                return self.to_seconds() <= other
+            return self.to_seconds() <= other.to_seconds()
+        else:
+            raise TypeError(f"'<=' not supported between instances "
+                            f"of Clock and {other.__class__.__name__}")
+
+    def __eq__(self, other):
+        if isinstance(other, (Clock, int)):
+            if isinstance(other, int):
+                return self.to_seconds() == other
+            return self.__hour == other.__hour and \
+                self.__minute == other.__minute and \
+                self.__second == other.__second
+        else:
+            raise TypeError(f"'==' not supported between instances "
+                            f"of Clock and {other.__class__.__name__}")
+
+    def __ne__(self, other):
+        if isinstance(other, (Clock, int)):
+            if isinstance(other, int):
+                return self.to_seconds() != other
+            return self.to_seconds() != other.to_seconds()
+        else:
+            raise TypeError(f"'!=' not supported between instances "
+                            f"of Clock and {other.__class__.__name__}")
+
+    def __gt__(self, other):
+        if isinstance(other, (Clock, int)):
+            if isinstance(other, int):
+                return self.to_seconds() > other
+            return self.to_seconds() > other.to_seconds()
+        else:
+            raise TypeError(f"'>' not supported between instances "
+                            f"of Clock and {other.__class__.__name__}")
+
+    def __ge__(self, other):
+        if isinstance(other, (Clock, int)):
+            if isinstance(other, int):
+                return self.to_seconds() >= other
+            return self.to_seconds() >= other.to_seconds()
+        else:
+            raise TypeError(f"'>=' not supported between instances "
+                            f"of Clock and {other.__class__.__name__}")
+
+    def __hash__(self):
+        return hash((self.__hour, self.__minute, self.__second))
+
+    def __bool__(self):
+        """ Возвращает False если полночь """
+        return self.to_seconds() != 0
+
+    def __int__(self):
+        return self.__hour * 3600 + self.__minute * 60 + self.__second
+
