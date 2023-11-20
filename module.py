07d877d6bea22847c77ac05a710b7b8d5a7346a1
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2023-11-20 19:45:10 +0300

    Добавил агрегацию

---
+++ module.py
@@ -0,0 +1,114 @@
+from copy import copy, deepcopy
+import random
+
+"""
+Взаимодействие между классами.
+Есть 3 способа:
+    1. Наследование. Класс-наследник имеет все поля и методы 
+    родителя и добавляет какой-то новый функционал.
+    Описывается словом "является". 
+    
+    2. Ассоциация. Класс в качестве полей использует другой класс
+    
+    class Engine: ...
+    class Car:
+        engine: Engine
+        
+    Описывается словом "имеет". Автомобиль имеет двигатель.
+    
+    class Car(Engine): - миксины
+        ...
+        
+        2.1 Агрегация ("Слабая ассоциация"). Тип взаимодействия, когда 
+        зависимая структура может существовать отдельно от родительской.
+
+"""
+
+
+class Book:
+
+    def __init__(self, name: str):
+        self.__name = name
+
+
+class Reader:
+
+    def __init__(self, name: str, number: str):
+        self.__name = name
+        self.__number = number
+        self.__books: list[Book] = []
+
+    def add_book(self, b: Book):
+        self.__books.append(b)
+
+
+class Library:
+
+    def __init__(self):
+        self.__books: list[Book] = []
+        self.__readers: list[Reader] = []
+
+    def add_book(self, b: Book):
+        self.__books.append(b)
+
+    def give_book(self, b: Book, r: Reader):
+        if b in self.__books:
+            if r in self.__readers:
+                r.add_book(b)  # выдать читателю книгу
+                self.__books.remove(b)  # удалить книгу из библиотеки
+
+
+book = Book("Самоучитель по Python")
+library = Library()
+library.add_book(book)
+
+"""
+Между классами Library и Book возникает слабая ассоциация, т.е. 
+класс Library позаимствовал объект класса Book, в т.ч. класс Reader
+также позаимствовал объект класса Book.
+
+"""
+
+
+class Engine:
+    __status = False
+
+    def __init__(self, power: int):
+        self.__power = power
+
+    def get_status(self):
+        return self.__status
+
+    def on(self):
+        self.__status = True
+
+    def off(self):
+        self.__status = False
+
+
+class Car:
+
+    def __init__(self, engine: Engine):
+        self.__engine = engine
+
+    def start(self):
+        if self.__engine.get_status():
+            print("Двигатель уже заведен, можно ехать")
+        else:
+            self.__engine.on()
+            print("Двигатель заведен, поехали...")
+
+
+engine = Engine(120)
+car = Car(engine)
+car.start()
+
+"""
+Агрегация - это когда экземпляр класса engine был создан где-то извне
+и передается в конструктор класса Car в качестве параметра.
+
+
+        2.2 Композиция. 
+
+"""
+
